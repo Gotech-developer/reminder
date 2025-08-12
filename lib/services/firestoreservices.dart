848b150d126d15a5be540e5cc46877c009f6fc52
Godday <goddayaweriale1@gmail.com>
2025-08-12 09:58:39 -0700

    initial commit

--- lib/services/firestoreservices.dart
+++ lib/services/firestoreservices.dart
@@ -1,1 +1,26 @@
-class Firestoreservices {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:reminder/models/todoitem.dart';
+
+class Firestoreservices {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+
+  Future<void> sendToFireStore(
+    String date,
+    String time,
+    String title,
+    String body,
+  ) async {
+    final user = auth.currentUser;
+    if (user != null) {
+      final userId = user.uid;
+      firebaseFirestore
+          .collection('users')
+          .doc(userId)
+          .set(
+            Todoitem(date: date, time: time, title: title, body: body).toMap(),
+          );
+    }
+  }
+}
